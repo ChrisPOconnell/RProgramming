4aed3c81002f9156e626c0e818fe8af6124bc414
Chris P. OConnell <chrispoconnell@outlook.com>
2015-08-13 21:02:38 -0400

    Almost there on assignment 1

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,7 +1,8 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
-  directory <- "C:/Users/ChrisP/Documents/GitHub/RProgramming"
+  #directory <- "C:/Users/ChrisP/Documents/GitHub/specdata"
+  directory <-  "C:/Users/chris_000/Documents/GitHub/specdata/"
   
   ## 'pollutant' is a character vector of length 1 indicating
   ## the name of the pollutant for which we will calculate the
@@ -13,4 +14,15 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
+
+  i <- 0            #Set counter position
+  x <- length(id)   #Get end position for while loop
+ 
+  while(i < x) {
+    i <- i + 1        #Increase index counter
+    print(id[i])
+    
+    
+}
+   
 }
